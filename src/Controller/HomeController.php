0d3779c03ad806c2849d0b7179b7ce7659d97b81
Zyus <youssefzyus33@gmail.com>
2022-04-29 21:34:06 +0100

    :hammer: Add product feature

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,118 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $products = [
+            [
+                'name' => 'Fancy Product',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => 'Special Item',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+            [
+                'name' => '',
+                'image' => 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+                'price_min' => 0,
+                'price_max' => 0,
+                'price_through' => 0,
+                'sale' => null,
+            ],
+        ];
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products' => $products,
         ]);
     }
 }
---
+++ src/Entity/Product.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    #[ORM\Column(type: 'float')]
+    private $price;
+
+    #[ORM\Column(type: 'float')]
+    private $sale;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSale(): ?float
+    {
+        return $this->sale;
+    }
+
+    public function setSale(float $sale): self
+    {
+        $this->sale = $sale;
+
+        return $this;
+    }
+}
