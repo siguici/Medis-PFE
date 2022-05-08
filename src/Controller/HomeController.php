1e89f798d4859ffa61b1fdabbe68873ca2eaef84
Zyus <youssefzyus33@gmail.com>
2022-05-08 18:22:36 +0100

    :lipstick: Integrate new template: Moderna

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProductRepository;
@@ -10,11 +11,20 @@ use App\Repository\ProductRepository;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(Request $request, ProductRepository $productRepository): Response
     {
+        $session = $request->getSession();
+        $quantity = $request->query->get('quantity') ?: 0;
+        if ($quantity) {
+            $session->set('quantity', $quantity);
+        }
+        if(!$session->has('quantity')) {
+            $session->set('quantity', $quantity);
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'products' => $productRepository->findAll(),
+            'quantity' => $session->get('quantity'),
         ]);
     }
 }
